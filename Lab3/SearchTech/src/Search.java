3248ad200099be22ccca22f68f30f7734dbf41f8
Ethan McTiernan <ethan.mctiernan@gmail.com>
2015-11-21 01:19:43 +0000

    Added hash map and class for <value> Structure

--- Lab3/SearchTech/src/Search.java
+++ Lab3/SearchTech/src/Search.java
@@ -5,13 +5,29 @@
  */
 
 
+import sun.awt.image.ImageWatched;
+
 import java.net.*;
 import java.io.*;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
+
+class struct{
+
+    String word;
+    int frequency;
+    double idf;
+
+    void put(String wrd,int freq, double i_d_f){
+        word=wrd;
+        frequency= freq;
+        idf = i_d_f;
+    }
+}
 
 public class Search {
+
+    static HashMap<String, LinkedList<struct>> docName_length = new HashMap<>();
+
 
     static void downloadFile() throws IOException {
         BufferedInputStream in = null;
@@ -37,55 +53,99 @@ public class Search {
             }
         }
     }
-    //HashMap < String , int> words = new HashMap();
     static void getWords() throws FileNotFoundException {
-        String docName;                                     //Variable with the document name
-        boolean docNameRetrieved =false;
+        String docName = " ";//Variable with the document name
+        struct structure = new struct();
+        String word = "";
+        int frequency=0;
+        double idf=0;
+        boolean docNameRetrieved = false;
         boolean doneDoc = false;
         boolean docStart = false;
         Scanner reader = new Scanner(new FileInputStream("output.txt"));  //Read in the file
+        LinkedList<struct> list= new LinkedList<>();
         while (reader.hasNext()) {      // while there is another token to read
             String temp = reader.next(); //temp is equal to the next string
-            if (!docNameRetrieved ){    //If we haven't yet read the document name
-                String [] getDocStart = temp.split(">");  //split the string here
-                if(getDocStart[0].equals("target=\"_blank\""))  //this is what the start of the string looks like
+            if (!docNameRetrieved) {    //If we haven't yet read the document name
+                String[] getDocStart = temp.split(">");  //split the string here
+                if (getDocStart[0].equals("target=\"_blank\""))  //this is what the start of the string looks like
                 {
                     String[] docHeading = getDocStart[1].split("<"); //if its a match split at the '<'
                     docName = docHeading[0];   //document name is equal to whats at position 0
                     docNameRetrieved = true;    //tell the program we have got the doc name
-                    System.out.println("DocName: " + docName);  //just for testing purposes
+                    //System.out.println("DocName: " + docName);  //just for testing purposes
                 }
             }
-             if (temp.equals("Vector:") && !docStart){ //when we read the vector enter
+            if (temp.equals("Vector:") && !docStart) { //when we read the vector enter
                 String s = reader.next();
-                 docStart = true;       //let the program know we have started reading the words in the document
-                 String[] wordSplit;
-                wordSplit= s.split(">");
-                 String [] searchedWord=wordSplit[1].split(":");
-                 searchedWord[1] = searchedWord[1].substring(0, searchedWord[1].length()-1);
-                 s = reader.next();
-                 System.out.println(searchedWord[0] + " " + searchedWord[1] + " " +s);
-            }
-            else if(docStart){
-                    //read the rest of the words in the document
+                docStart = true;       //let the program know we have started reading the words in the document
+                String[] wordSplit;
+                wordSplit = s.split(">");
+                String[] searchedWord = wordSplit[1].split(":");
+                searchedWord[1] = searchedWord[1].substring(0, searchedWord[1].length() - 1);
+                word = searchedWord[1];
+                frequency = Integer.parseInt(searchedWord[1]);
+                idf = Double.parseDouble(reader.next());
+                structure.put(word, frequency, idf);
+                //System.out.println(searchedWord[0] + " " + searchedWord[1] + " " + idf);
+            } else if (docStart) {
+                //read the rest of the words in the document
 
-                 String[] wordSplit;
-                 wordSplit = temp.split(":");
+                String[] wordSplit;
+                wordSplit = temp.split(":");
 
-                 //System.out.println( " In loop 2 : "+wordSplit[0]);
-                 wordSplit[1] = wordSplit[1].substring(0, wordSplit[1].length()-1);
-                 String s = reader.next();
-                 if(s.contains("<")) { //when a string contatins the '<' we know we have reached the last word.
-                     String[] lastEntry = s.split("<"); // split it and let s = to the idf
-                     s = lastEntry[0];
-                     doneDoc = true;
-                 }
-                 System.out.println(wordSplit[0] + " " + wordSplit[1]+ " "+s);
+                //System.out.println( " In loop 2 : "+wordSplit[0]);
+                wordSplit[1] = wordSplit[1].substring(0, wordSplit[1].length() - 1);
+                word = wordSplit[0];
+                frequency = Integer.parseInt(wordSplit[1]);
+                String s = reader.next();
+                if (s.contains("<")) { //when a string contatins the '<' we know we have reached the last word.
+                    String[] lastEntry = s.split("<"); // split it and let s = to the idf
+                    idf = Double.parseDouble(lastEntry[0]);
+                    doneDoc = true;
+                }
+                else idf = Double.parseDouble(s);
+                //System.out.println(wordSplit[0] + " " + wordSplit[1] + " " + s);
+                structure.put(word,frequency,idf);
+                //System.out.println(structure.word + structure.idf + structure.frequency);
+                list.add(structure);
             }
-            if (doneDoc) {doneDoc=false;docStart = false; docNameRetrieved = false; System.out.println("____________________________________________________");}
+            if (doneDoc) {
+                doneDoc = false;
+                docStart = false;
+                docNameRetrieved = false;
+                System.out.println("List before its sent size" + list.size());
+                createList(docName, list);
+                list.clear();
+                //System.out.println("____________________________________________________");
+            }
+
 
 
         }
+
+        //System.out.println(docName_length);
+
+    }
+
+    static void printMap(){
+
+        for ( String key: docName_length.keySet()){
+
+            LinkedList <struct> fullList = docName_length.get(key);
+            System.out.println(key);
+            System.out.println(fullList.size());
+            for (struct st : fullList) {
+                System.out.println(st.word + " " + st.frequency + " " + st.idf);
+            }
+
+
+        }
+    }
+    static void createList(String doc_name,LinkedList<struct> str) {
+        struct test = str.get(0);
+        System.out.println("in createList size : " + test.word + test.frequency);
+        docName_length.put(doc_name, str);
 
     }
 
@@ -93,6 +153,7 @@ public class Search {
 
             //downloadFile(); commented it out because need to be on campus to access url
             getWords();
+            printMap();
 
     }
 
